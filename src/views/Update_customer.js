7f551cab56ed0f45a4b81748b51330c5e743b3d8
Roberto Villanueva Cab <rvillanueva@imasd.com.mx>
2019-07-17 21:53:10 -0500

    guardado ok

--- src/views/Update_customer.js
+++ src/views/Update_customer.js
@@ -16,8 +16,7 @@ query Customers($id:ID!) {
     first_name,
     last_name,
     email,
-    telephone,
-    profile_picture
+    telephone
   }
 }
 `;
@@ -26,8 +25,7 @@ mutation updateCustomers($id:ID!,$data:updateCustomerInput!){
     updateCustomers(id:$id,data:$data){
       first_name,
       last_name,
-      telephone,
-      profile_picture
+      telephone
     }
   }
 `;
@@ -42,27 +40,23 @@ function Update_Customer({history}){
     const {data,loading} = useQuery(READ_CUSTOMER,{variables:{id:idcustom}})
     //console.log(data);
 
-    const [profile_picture,setProfilePicture]=useState('');
-    const [ProfilePreview,setProfilePreview]=useState('');
     const [user,setUser]=useState('');
 
     const [sendCustomer,{error}]=useMutation(UPDATE_CUSTOMER);
     
 
-    const handleCover=event=>{
-        const render =new FileReader();
-        const file =event.target.files[0];
-        render.onloadend=()=>{
-            setProfilePicture(file)
-            setProfilePreview(render.result)
-        }
-
-        render.readAsDataURL(file);
-    }
-
     const catchRegistro=async(fields)=>{
-        await sendCustomer({variables:{id:idcustom,data:{...fields,profile_picture}}})
-        error ? alert("Hubo un error") : history.push('/login')
+        delete fields.__typename
+        delete fields._id
+        await sendCustomer({variables:{id:idcustom,data:{...fields}}})
+
+        if(error){
+            alert("Hubo un error") 
+        }
+        else{
+            localStorage.removeItem('UberToken')
+            history.push('/login')
+        } 
         
     }
 
@@ -108,22 +102,6 @@ function Update_Customer({history}){
                         onChange={handleInputChange}
                         required
                         />
-                         <div className="control-group">
-                            <div className="form-group">
-                                <div className="input-group">
-
-                                <img src={ProfilePreview} alt="Vista previa" className="col-md-3 Perfil"/>
-                                <Input name="profile_picture"
-                                    label="Foto de perfil"
-                                    type="file"
-                                    placeholder="Seleccione la foto del perfil"
-                                    onChange={handleCover}
-                                    required
-                                    />
-                                </div>
-                            </div>
-                        </div>    
-                        
                         <input type="submit" className="fadeIn fourth col-md-9" value="Enviar"/>
                     </form>
                 </div>
